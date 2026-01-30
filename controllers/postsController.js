64f7439113eab0dc3b3b7a5cd2d2af9814a8632a
ofekn199 <ofekng199@gmail.com>
2026-01-30 11:11:51 +0200

    Add create post endpoint

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,5 +1,30 @@
 const postsModel = require("../models/postsModel");
 
+// Create a new post
+const createPost = async (req, res) => {
+    try {
+        const { title, sender, content } = req.body;
+
+        // Basic validation
+        if (!title || !sender || !content) {
+            return res.status(400).json({ message: "Missing required fields" });
+        }
+
+        const newPost = new postsModel({
+            title,
+            sender,
+            content,
+        });
+
+        await newPost.save();
+
+        res.status(201).json(newPost);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Error creating post" });
+    }
+};
+
 // Get all posts
 const getAllPosts = async (req, res) => {
     try {
@@ -35,6 +60,7 @@ const getPostById = async (req, res) => {
 };
 
 module.exports = {
+    createPost,
     getAllPosts,
     getPostById,
 };
