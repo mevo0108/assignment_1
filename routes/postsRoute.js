069e076d2786655ce9617017ed5ada7daa022cc9
ofekn199 <ofekng199@gmail.com>
2026-02-01 11:28:40 +0200

    update Post

--- routes/postsRoute.js
+++ routes/postsRoute.js
@@ -3,6 +3,7 @@ const {
     createPost,
     getAllPosts,
     getPostById,
+    updatePost,
 } = require("../controllers/postsController");
 
 const { getCommentsByPost } = require("../controllers/commentsController");
@@ -20,5 +21,8 @@ router.get("/:id", getPostById);
 
 // Get comments By post ID
 router.get("/:postId/comments", getCommentsByPost);
+
+// Update post by id
+router.put("/:id", updatePost);
 
 module.exports = router;
